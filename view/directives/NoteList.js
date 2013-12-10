70fe69e96c147b72bd0b0d9aeb85ad3f9fe66379
sifeij <sifeij@gmail.com>
2013-12-10 15:39:52 -0800

    User shortcut notation in directive's scope

--- view/directives/NoteList.js
+++ view/directives/NoteList.js
@@ -3,9 +3,9 @@ Guidebook.directive('gbNoteList', function(){
 		restrict: 'E',
 		templateUrl: 'view/directives/noteList.html',
 		scope: {
-			show: '=show',
-			notes: '=notes',
-			orderValue: '@orderBy',
+			show: '=',							// "=" pass teh value as-is, it's useful for arrays and functions
+			notes: '=',							// "&" will render the content as an expression
+			orderValue: '@orderBy',				// "@" will render the given information as a string
 			onDelete: '=deleteNoteHandler'
 		}
 	};
