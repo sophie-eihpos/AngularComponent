abbcbf04372961d7b1beff44119543b9c2b15988
sifeij <sifeij@gmail.com>
2013-12-10 15:37:34 -0800

    Define scope for directive

--- view/directives/NoteList.js
+++ view/directives/NoteList.js
@@ -1,6 +1,12 @@
 Guidebook.directive('gbNoteList', function(){
 	return {
 		restrict: 'E',
-		templateUrl: 'view/directives/noteList.html'
+		templateUrl: 'view/directives/noteList.html',
+		scope: {
+			show: '=show',
+			notes: '=notes',
+			orderValue: '@orderBy',
+			onDelete: '=deleteNoteHandler'
+		}
 	};
 });
